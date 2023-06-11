d9817c6a5ffdc08e648ca710683bc8eb7ece9282
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2023-06-11 19:06:38 +0530

    database errors cleared

--- students/views.py
+++ students/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Students
+from .models import Student
 from django.urls import reverse
 # Create your views here.
 
 def index(request):
     return render(request, 'students/index.html',{
-        'students': Students.objects.all()
+        'students': Student.objects.all()
     })
 
 def view_student(request):
-    student = Students.objects.get(pk=id)
+    student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
